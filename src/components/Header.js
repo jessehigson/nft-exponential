b799aa637abf6d1fc1ddf8a7235f24c119a7ff87
Jesse Higson <jesse@superrb.com>
2021-11-13 12:41:34 +0000

    Adding logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
+import { Link } from "gatsby"
 import * as React from "react"
+import LogoImage from "../assets/icons/logo.svg"
 
 export const Header = () => {
   return (
     <header className="header">
       <div className="header__container container container--flex">
-        <h1 className="header__title">nft.exponential</h1>
+        <Link to="/" className="header__logo logo">
+          <LogoImage />
+          <h1 className="screenreader-text">nft.exponential</h1>
+        </Link>
       </div>
     </header>
   )
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,7 +3,7 @@ import * as React from "react"
 export const MainContent = ({ title }) => (
   <section className="main-content">
     <div className="main-content__container container container--flex">
-      <h1 className="main-content__title">{title}</h1>
+      <h2 className="main-content__title">{title}</h2>
     </div>
   </section>
 )
